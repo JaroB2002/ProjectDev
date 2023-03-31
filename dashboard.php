a2fb507ed209333fe980a55066a20194fe3a1e07
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-03-31 18:55:57 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,27 @@
+<?php 
+    session_start();
+    if(isset($_SESSION['username'])){
+        //user is logged in
+        echo "Welcome " . $_SESSION['username'];
+        //queries in sql
+    } else{
+        //user is not logged in
+        header("location: login.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<a href="logout.php">Log out?</a>
+
+<body>
+    <h1>Your home</h1>
+</body>
+</html>
 
