302a34e1ffa16935cbcd662343d9ef80e15ca054
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-03-31 20:16:07 +0200

    Update dashboard.php
    Delete account btn

--- dashboard.php
+++ dashboard.php
@@ -25,3 +25,22 @@
 </body>
 </html>
 
+
+<h2>Delete Your Account</h2>
+<p>Please confirm your email address to delete your account and all associated data.</p>
+<form method="post" action="dashboard.php">
+  <label for="email">Email:</label>
+  <input type="email" name="email" required>
+  <br>
+  <label for="confirm_delete">Type your email address to confirm:</label>
+  <input type="email" name="confirm_delete" required>
+  <br>
+  <p>By clicking the button below, you will permanently delete your account and all associated data.</p>
+  <button type="submit" name="delete_account" onclick="return confirm('Are you sure you want to delete your account? This action cannot be undone.');">Delete Account</button>
+</form>
+
+<h2>Download Your Data</h2>
+<p>You can download a copy of your data by clicking the button below.</p>
+<a href="download_data.php">Download My Data</a>
+
+
