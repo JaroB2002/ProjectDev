88975ff94151a145547483c9d9feb9f4cd05788f
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-03-31 20:13:14 +0200

    Update download_data.php
    Download data works as a text file

--- download_data.php
+++ download_data.php
@@ -8,3 +8,21 @@ $stmt = $conn->prepare('SELECT * FROM users');
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// Set headers for CSV file download
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="users.csv"');
+
+// Create file pointer for output
+$output = fopen('php://output', 'w');
+
+// Write headers to CSV file
+fputcsv($output, array_keys($results[0]));
+
+// Write data to CSV file
+foreach ($results as $row) {
+    fputcsv($output, $row);
+}
+
+// Close file pointer
+fclose($output);
+?>
