84bb34cf5e2a9f5f6a58bfcd3a9c35258a4c3c6c
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-03-31 19:25:16 +0200

    Update download_data.php

--- download_data.php
+++ download_data.php
@@ -1,1 +1,10 @@
 
+<?php
+// Establish database connection
+$conn = new PDO('mysql:host=localhost;dbname=demo', 'root', '');
+
+// Retrieve data from database
+$stmt = $conn->prepare('SELECT * FROM users');
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
