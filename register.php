a9b07b9042615d840bf8833a36df6b47d602639f
YadinaM <yadina.moreira@gmail.com>
2023-04-27 15:34:42 +0200

    bugfix: sendEmail functie in comment gezet

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
         $user->setEmail($_POST["email"]);
         $user->setPassword($_POST["password"]);
         $user->save();
-        $user->sendMail();
+        //$user->sendMail();
 
         /*session_start(); 
         $id = $_SESSION['id'];*/
