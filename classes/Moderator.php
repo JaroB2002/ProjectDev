245877ac291654193f3b3d26d707d950872c641a
YadinaM <yadina.moreira@gmail.com>
2023-05-02 13:05:44 +0200

    chore: functies naar moderator class verplaatst

--- classes/Moderator.php
+++ classes/Moderator.php
@@ -3,4 +3,18 @@
 
     class Moderator extends User {
        
+        public function approvePrompt(){
+            $conn = Db::getInstance();
+            $statement = $conn->prepare('update prompts set approved = :approve where id = :id');
+            $statement->bindValue(':approve', 1);
+            $statement->bindValue(":id", $_GET["approve"]);
+            $statement->execute();
+        }
+
+        public function unapprovePrompt(){
+            $conn = Db::getInstance();
+            $statement = $conn->prepare('delete from prompts where id = :id');
+            $statement->bindValue(":id", $_GET["disapprove"]);
+            $statement->execute();
+        }
     }
