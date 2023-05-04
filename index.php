d48ffb59859b48670c65585125544e6fe28e159e
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-05-04 11:16:05 +0200

    Bugfix: layout changes login

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
-function canLogin($username, $password){
+function canLogin($email, $password){
   require_once 'classes/Db.php'; // Include the file containing the definition of the Db class
   $conn = Db::getInstance(); // Create an instance of the Db class
   $statement = $conn->prepare("select * from users where email= :email");
-  $statement->bindValue(":email", $username);
+  $statement->bindValue(":email", $email);
   $statement->execute();
   $user = $statement->fetch(PDO::FETCH_ASSOC); // Fetch as associative array
   if(!$user){
@@ -21,14 +21,14 @@ function canLogin($username, $password){
 
 if(!empty($_POST)){
   //er is verzonden
-  $username= $_POST['username'];
+  $email= $_POST['email'];
   $password= $_POST['password'];
   $rememberMe = isset($_POST['remember-me']) ? true : false; // Check if the checkbox is checked
 
-  if(canLogin($username, $password)){
+  if(canLogin($email, $password)){
     //inloggen
     session_start();
-    $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
 
     if ($rememberMe) {
       // Store the value in a cookie
@@ -41,9 +41,59 @@ if(!empty($_POST)){
     $error = true;
   }
 }
+
+/*
+require_once 'classes/Db.php'; // Include the file containing the definition of the Db class
+public function canLogin($username, $password){
+  $conn = Db::getInstance(); // Create an instance of the Db class
+  $statement = $conn->prepare("select * from users where email= :email");
+  $statement->bindValue(":email", $this->getEmail());
+  $statement->execute();
+  $user = $statement->fetch(PDO::FETCH_ASSOC); // Fetch as associative array
+  if(!$user){
+    return false;
+  }
+
+  $hash = $user[$this->getPassword()];
+
+  if(password_verify($password, $hash) ){
+    return true;
+  } else{
+    return false;
+  }
+}
 ?>
 
+*/
+if(!empty($_POST)){
+  try{
+    //er is verzonden
+    $user = new User();
+    $user->setEmail($_POST["email"]);
+    $user->setPassword($_POST["password"]);
 
+    if($user->canLogin()){
+      //inloggen
+      session_start();
+      $_SESSION['email'] = $user->getEmail();
+
+      $rememberMe = isset($_POST['remember-me']) ? true : false; // Check if the checkbox is checked
+      if ($rememberMe) {
+        // Store the value in a cookie
+        setcookie("remember_me", "1", time() + (86400 * 30), "/"); // 30 days expiration time
+      }
+
+      header("Location: dashboard.php");
+    } else{
+      //error
+      $error = true;
+    }
+  }
+  catch (Throwable $e){
+    $error = $e->getMessage();
+  }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en" >
@@ -77,62 +127,63 @@ if(!empty($_POST)){
 <!-- partial:index.partial.html -->
 <header>
     <a href="#" class="loggedIn">
-     <?php if(isset($_SESSION['username'])): ?>
-      <h3 class="user--name"><?php echo $_SESSION['username']; ?></h3>
+     <?php if(isset($_SESSION['email'])): ?>
+      <h3 class="user--name"><?php echo $_SESSION['email']; ?></h3>
       <?php endif; ?>
 
     </a>
   </nav>    
 </header>
 
-<div class="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-  <div class="w-full max-w-md space-y-8">
-    <div>
-      <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Logo project">
-      <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-      <p class="mt-2 text-center text-sm text-gray-600">
-        Or
-        <a href="register.php" class="font-medium text-indigo-600 hover:text-indigo-500">register now</a>
-      </p>
-    </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
-      <input type="hidden" name="remember" value="true">
-      <div class="-space-y-px rounded-md shadow-sm">
-        <div>
-          <label for="username" class="sr-only">Email address</label>
-          <input id="username" name="username" type="text" autocomplete="username" required class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address">
-        </div>
-        <div>
-          <label for="password" class="sr-only">Password</label>
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password">
-        </div>
-      </div>
+<div class="relative md:h-screen flex overflow-hidden">
+   <div class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center flex-auto min-w-0 bg-white md:my-0 my-8">
+     <div class="md:flex md:items-center md:justify-center w-full  md:h-full sm:rounded-lg md:rounded-none bg-white px-6">
+       <div class="max-w-md w-full mx-auto">
+         <div>
+           <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Logo project">
+           <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Log in</h2>
+           <p class="mt-2 text-center text-sm text-gray-600"> Of <a href="register.php" class="font-medium text-indigo-600 hover:text-indigo-500">registreer</a> je nu
+           </p>
+         </div>
+         <form class="mt-8 space-y-6" action="#" method="POST">
+           <input type="hidden" name="remember" value="true">
+           <div class="-space-y-px rounded-md shadow-sm">
+             <div>
+               <label for="email" class="sr-only">Email address</label>
+               <input id="email" name="email" type="text" autocomplete="email" required class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address">
+             </div>
+             <div>
+               <label for="password" class="sr-only">Wachtwoord</label>
+               <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password">
+             </div>
+           </div>
+           <div class="flex items-center justify-between">
+             <div class="flex items-center">
+               <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+               <label for="remember-me" class="ml-2 block text-sm text-gray-900">Onthoud mijn gegevens</label>
+             </div>
+             <div class="text-sm">
+               <a href="reset_password.php" class="font-medium text-indigo-600 hover:text-indigo-500">Wachtwoord vergeten?</a>
+             </div>
+           </div>
+           <div>
+             <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+               <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                 <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                   <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                 </svg>
+               </span> Log in </button>
+           </div>
+         </form>
+       </div>
+     </div>
+   </div>
+   <div class="sm:w-2/4 hidden md:flex">
+     <img class="w-full object-cover" src="https://images.pexels.com/photos/3182750/pexels-photo-3182750.jpeg" alt="">
+   </div>
+ </div>
+ </div>
 
-      <div class="flex items-center justify-between">
-        <div class="flex items-center">
-          <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-          <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-        </div>
-
-        <div class="text-sm">
-          <a href="reset_password.php" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
-        </div>
-      </div>
-
-      <div>
-        <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-            </svg>
-          </span>
-          Sign in
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
-</div>
 <!-- partial -->
 
   
