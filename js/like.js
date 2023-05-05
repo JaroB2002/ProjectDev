e0761ae41031da333a8f247038c576f93030d55f
YadinaM <yadina.moreira@gmail.com>
2023-05-05 15:33:28 +0200

    feat: bij klik op like weet die welke id de propt heeft

--- js/like.js
+++ js/like.js
@@ -1,1 +1,18 @@
-console.log("hallo");
+console.log("link");
+
+let links = document.querySelectorAll(".like");
+for(let i = 0; i < links.length; i++){
+    links[i].addEventListener("click", function(e){
+        e.preventDefault();
+        console.log("geklikt😅");
+
+        //krijg de id voor de prompt
+        let id = this.getAttribute("data-id");
+
+        //de spam met de id likes[id]
+        let span = document.querySelector("#likes" + id);
+        console.log(id);
+
+
+    });
+}
