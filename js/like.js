aa837f320606b00d48605f0182f35033ddb76886
YadinaM <yadina.moreira@gmail.com>
2023-05-06 14:41:34 +0200

    feat: ajax deel toegevoegd -> krijg nog json error dus werkt niet

--- js/like.js
+++ js/like.js
@@ -1,16 +1,36 @@
-console.log("link");
+//console.log("link");
 
 let links = document.querySelectorAll(".like");
 for(let i = 0; i < links.length; i++){
     links[i].addEventListener("click", function(e){
         e.preventDefault();
-        console.log("geklikt😅");
+        //console.log("geklikt😅");
 
         //krijg de id voor de prompt
-        let id = this.getAttribute("data-id");
+        let promptId = this.getAttribute("data-id");
 
         //de spam met de id likes[id]
-        let span = document.querySelector("#likes" + id);
-        console.log(id);
+        let span = document.querySelector("#likes" + promptId);
+        console.log(promptId);
+
+        //post naar database AJAX
+        let formData = new  FormData();
+        formData.append("data-id",promptId );
+
+        fetch("ajax/like.php", {
+                method: "POST", // or 'PUT'
+                body: formData
+        })
+
+        .then(response => response.json())
+        .then(result =>{
+            console.log("succes", result);
+        })
+        .catch(error => {
+            console.log("error", error);
+        })
+        
+          
+        //aantal likes tonen 
     });
 }
