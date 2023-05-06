a54348bf54a1fb22a138fa4153da9c6e8d12a962
YadinaM <yadina.moreira@gmail.com>
2023-05-06 14:42:53 +0200

    feat: ajax onderdelen toegevoegd, krijg json error

--- ajax/like.php
+++ ajax/like.php
@@ -1,4 +1,21 @@
 <?php
     include_once("bootstrap.php");
+    if(!empty($_POST)){
+         //nieuwe like aanmaken
+        $l = new Like();
+        $l->setPromptId($_POST["data-id"]);
+        $l->setUser($_SESSION["username"]);
 
+         //save()
+        $l->save();
+
+         //succesboodschap teruggeven
+         $respons = [
+             'status' => 'succes',
+             'message' => 'like saved',
+         ];
+
+         header('Content-Type: application/json');
+         echo json_encode($respons); //json object gemaakt worden 
+    }
 ?>
