0240e213e74f715382ec7c5daa0e8b0293e54eba
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-05-15 19:23:10 +0200

    fix: bring back report prompt lost in merge conflict

--- dashboard.php
+++ dashboard.php
@@ -203,11 +203,39 @@ $allComments = Comment::getAll(3);
 </div>
   <script src="index.css"></script>
   <script src="app.js"></script>
+  <script>
+        let report = document.querySelectorAll("#reportButton");
 
+        report.forEach(function(button){
+            button.addEventListener("click", reportPrompt);
+        });
 
+        function reportPrompt(event){
+        console.log(event);
+        event.preventDefault();
+        console.log("reportPrompt");
+        let promptid = event.target.dataset.promptid;
+        console.log(promptid);
+        let formData = new FormData();
+        formData.append("promptid", promptid);
+
+        let item = this;
+        fetch("ajax/reportPrompt.php", {
+            method: "POST",
+            body: formData
+        })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(result){
+            if(result.status == "success"){
+                item.innerHTML = result.message;
+            }
+        });
+        }
+    </script>
 </body>
 </html>
-<a href="#" data-id="<?php echo $prompt['id']; ?>" class="like">Like <span class='likes' id="likes"><?php echo $likes->getLikes($prompt['id']) ?> people like this</span> </a>
 <?php
 // START DELETE ACCOUNT CODE
 
