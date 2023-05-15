232fae91c8f3fca19c0c70d8957256becef450e1
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-05-15 20:29:53 +0200

    Chore: codecleaning

--- editprofile.php
+++ editprofile.php
@@ -60,42 +60,50 @@
   </script>
 </head>
 <body>
-    <!--mx margin left right, my margin top bottom-->
-    <nav class="relative container mx-auto p-6 bg-offgrey rounded-md">
+  <!--mx margin left right, my margin top bottom-->
+  <nav class="relative container mx-auto p-6 bg-offgrey rounded-md">
     <!--flex container-->
     <div class="flex items-center justify-between">
-        <!--menu-->
-        <div class="hidden md:flex space-x-6">
-            <a href="dashboard.php" class="text-lg font-bold hover:text-fadedpurple">Home</a>
-            <a href="editprofile.php" class="text-lg font-bold hover:text-fadedpurple">Profile</a>
-            <a href="uploadPrompt.php" class="text-lg font-bold hover:text-fadedpurple">Upload</a>
-        </div>
-        <!--button-->
-        <a href="logout.php" class="hidden md:block p-3 px-6 pt-2 text-white bg-fadedpurple rounded-full baseline">Log out</a>
+      <!--menu-->
+      <div class="hidden md:flex space-x-6">
+        <a href="dashboard.php" class="text-lg font-bold hover:text-fadedpurple">Home</a>
+        <a href="editprofile.php" class="text-lg font-bold hover:text-fadedpurple">Profile</a>
+        <a href="uploadPrompt.php" class="text-lg font-bold hover:text-fadedpurple">Upload</a>
+      </div>
+      <!--button-->
+      <a href="logout.php" class="hidden md:block p-3 px-6 pt-2 text-white bg-fadedpurple rounded-full baseline">Log out</a>
     </div>
-    </nav>
-    <main class="ml-10 mt-10">
-      <h1 class="text-5xl mb-10 font-semibold">Profile</h1>
-      <h2 class="text-3xl text-fadedblue mb-5">Hi it's <?php echo htmlspecialchars($username); ?>!</h2>
-      <?php if($user->checkVerify()): ?>
-        <h2 class="text-2xl text-fadedpurple mb-5">I am a verified user.</h2>
-      <?php endif; ?>
-      <h3 class="text-2xl mb-3"><?php echo "My credits:" . " " . htmlspecialchars($credits);?></h3>
-      <div class="bg-offwhite rounded w-96 p-5">
+  </nav>
+  <main class="ml-10 mt-10">
+    <h1 class="text-5xl mb-10 font-semibold">Profile</h1>
+    <h2 class="text-3xl text-fadedblue mb-5">Hi it's <?php echo htmlspecialchars($username); ?>!</h2>
+    <?php if($user->checkVerify()): ?>
+      <h2 class="text-2xl text-fadedpurple mb-5">I am a verified user.</h2>
+    <?php endif; ?>
+    <h3 class="text-2xl mb-3"><?php echo "My credits:" . " " . htmlspecialchars($credits);?></h3>
+    <div class="bg-offwhite rounded w-96 p-5">
       <h3 class="text-2xl mb-3">My biography:</h3>
       <p class="text-8sm mb-3"><?php echo htmlspecialchars($biography); ?></p> 
+    </div>
+    <form action="#" method="post">
+      <div>
+        <label class="underline text-fadedblue text-1xl" for="biography">Update your biography here: </label>
+        <input id="biography" name="biography" type="text" placeholder="Biography">
       </div>
-      <form action="#" method="post">
-          <div>
-            <label class="underline text-fadedblue text-1xl" for="biography">Update your biography here: </label>
-            <input id="biography" name="biography" type="text" placeholder="Biography">
-          </div>
-          <div>
-          <button class="bg-fadedpurple px-5 py-3 mt-5 rounded font-semibold text-white" type="submit" name="updateProfile">Add bio</button>
-          </div>
-      </form>
-    </main>
+      <div>
+        <button class="bg-fadedpurple px-5 py-3 mt-5 rounded font-semibold text-white" type="submit" name="updateProfile">Add bio</button>
+      </div>
+    </form>
+  </main>
 </body>
+
+<form action="" method="post" enctype="multipart/form-data" class="ml-10">
+  <label>Selecteer een afbeelding:</label>
+  <input type="file" name="image" required>
+  <br><br>
+  <input type="submit" name="uploadPhoto" value="Uploaden" class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded">
+  <input type="submit" name="deletePhoto" value="Verwijderen" class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded">
+</form>
 </html>
 
 <?php
@@ -122,11 +130,4 @@ if ($profile->getProfilePhoto() != '') {
 }
 ?>
 
-<form action="" method="post" enctype="multipart/form-data" class="ml-10">
-  <label>Selecteer een afbeelding:</label>
-  <input type="file" name="image" required>
-  <br><br>
-  <input type="submit" name="uploadPhoto" value="Uploaden" class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded">
-<input type="submit" name="deletePhoto" value="Verwijderen" class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded">
-</form>
 <?php include_once("footer.php");?>
