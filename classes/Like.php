d690a1099f2edcfc6c453b4105284b2e968acf53
YadinaM <yadina.moreira@gmail.com>
2023-05-16 12:06:27 +0200

    feat: like and unlike prompt

--- classes/Like.php
+++ classes/Like.php
@@ -51,5 +51,28 @@
             return $statement->execute();
         }
 
+        public static function getAll($prompt_id)
+        {
+                $conn = Db::getInstance();
+                $statement = $conn->prepare("SELECT * FROM likes WHERE prompts_id = :prompts_id AND username = :username");
+                $statement->bindValue(":prompts_id", $prompt_id);
+                $statement->bindValue(":username", $_SESSION['username']);
+                $statement->execute();
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+                if (!empty($result)) {
+                        return true;
+                } else {
+                        return false;
+                }
+        }
+
+        public function remove($prompt_id)
+        {
+                $conn = Db::getInstance();
+                $statement = $conn->prepare("DELETE FROM likes WHERE prompts_id = :prompts_id AND username = :username");
+                $statement->bindValue(":prompts_id", $prompt_id);
+                $statement->bindValue(":username", $_SESSION['username']);
+                $statement->execute();
+        }
+
     }
-?>
