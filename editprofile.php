cacb04ca153d6f0288fbb533d838dd74b4e7389c
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-05-20 09:31:29 +0200

    Bugfix: code lost editprofile

--- editprofile.php
+++ editprofile.php
@@ -1,106 +1,155 @@
-<div class="bg-gray-900">
-  <div class="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-    <div class="grid row-gap-10 mb-8 lg:grid-cols-6">
-      <div class="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+<?php
+include_once("bootstrap.php");
 
-        <div>
-          <p class="font-medium tracking-wide text-gray-300">Our team</p>
-          <ul class="mt-2 space-y-2">
-            <li>
-              <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Jaro</a>
-            </li>
-            <li>
-              <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Sarah-Lisa</a>
-            </li>
-            <li>
-              <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Yadina</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <p class="font-medium tracking-wide text-gray-300">Promptbaes</p>
-          <ul class="mt-2 space-y-2">
-            <li>
-              <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Is</a>
-            </li>
-            <li>
-              <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">The</a>
-            </li>
-            <li>
-              <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Best</a>
-            </li>
-            <li>
-              <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Promptwebsite</a>
-            </li>
-          </ul>
-        </div>
+session_start();
+$username = $_SESSION['username'];
+
+$user = new User();
+$user->setEmail($username);
+$userDetails = $user->getUserDetails();
+$biography = $userDetails['biography'];
+
+// Profiel delen
+$currentDomain = $_SERVER['HTTP_HOST'];
+$profileLink = "https://" . $currentDomain . "/user.php?id=" . urlencode($username);
+
+if($user->checkVerify()){
+  $user->verifyUser(true);
+}
+
+if(!empty($_POST)){
+    try{
+        $user->setBiography($_POST['biography']);
+        $biography = $user->getBiography();
+        $user->updateProfile();
+    } 
+    catch(Throwable $e){
+        $error=$e->getMessage();
+    }
+}
+
+$allCredits = $user->showCredits();
+$credits = $allCredits['credits'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-PmkEJHmZvcwdeUDzL5Z+K9QGQxxbivn5nMxvM5rPLnAR">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+      theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+        },
+        extend: {
+          colors: {
+            fadedpurple: '#C688F4',
+            fadedblue: '#5C69AA',
+            offgrey: '#D9D9D9',
+            offblack: '#D9D9D9',
+            offwhite: '#f9f9f9'
+          }
+        }
+      }
+    }
+  </script>
+</head>
+<body>
+  <nav class="relative container mx-auto p-6 bg-offgrey rounded-md">
+    <div class="flex items-center justify-between">
+      <div class="hidden md:flex space-x-6">
+        <a href="dashboard.php" class="text-lg font-bold hover:text-fadedpurple">Home</a>
+        <a href="editprofile.php" class="text-lg font-bold hover:text-fadedpurple">Profile</a>
+        <a href="uploadPrompt.php" class="text-lg font-bold hover:text-fadedpurple">Upload</a>
       </div>
-      <div class="md:max-w-md lg:col-span-2">
-        <span class="text-base font-medium tracking-wide text-gray-300">Subscribe for updates</span>
-        <form class="flex flex-col mt-4 md:flex-row" method="post" action="subscribe.php">
-          <input
-            placeholder="Email"
-            required=""
-            type="text"
-            name="email"
-            class="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-          />
-          <button
-            type="submit"
-            name="subscribe"
-            class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-          >
-            Subscribe
-          </button>
-        </form>
-        <p class="mt-4 text-sm text-gray-500">
-          Wij zijn Promptbaes en dit is onze prachtige promptagina.
-        </p>
-      </div>
+      <a href="logout.php" class="hidden md:block p-3 px-6 pt-2 text-white bg-fadedpurple rounded-full baseline">Log out</a>
     </div>
-    <div class="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
-      <p class="text-sm text-gray-500">
-        © 2023 Promptbaes. Alle rechten voorbehouden.
-      </p>
-      <div class="flex items-center mt-4 space-x-4 sm:mt-0">
-        <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">
-          <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
-            <path
-              fill-rule="evenodd"
-              d="M12 2C6.477 2 2 6.477 2 12c0 4.42 3.582 8 8 8s8-3.58 8-8c0-5.523-4.477-10-10-10zm0 16c-3.314 0-6-2.686-6-6 0-3.313 2.686-6 6-6 3.313 0 6 2.687 6 6 0 3.314-2.687 6-6 6zm1-5.414v2.414h2v-2.414c0-.782-.613-1.414-1.415-1.414H13V8h-2v3.586H9.414C8.613 11.586 8 12.218 8 13v1h2v-1h1.586C12.387 11.586 13 10.954 13 10.172zM12 10h-1v1h1v-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
-        <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">
-          <svg viewBox="0 0 30 30" fill="currentColor" class="h-6">
-            <circle cx="15" cy="15" r="4"></circle>
-            <path
-              fill-rule="evenodd"
-              d="M18.879 10.769c-.271-.094-1.152-.334-2.352-.334-2.341 0-3.943 1.693-3.943 4.706 0 .883.096 1.582.334 2.116-.043.407-.134 1.271-.134 1.271s-.056.458-.272.635c-.296.268-.915.224-.915.224s-.658.083-1.261-.408c-.594-.474-.618-1.387-.618-1.756v-2.907c0-.387.277-.356.277-.356s1.028.125 1.565.545c.357.28.404.209.404.209s.108-.667.167-.978c.058-.309.14-.614.14-.929 0-2.166-1.35-2.728-2.66-2.728-1.232 0-1.807.184-2.201.283v1.695h-.59c-.002.42-.007.846-.007 1.27 0 .424.005.851.007 1.27h.59v4.025c0 .207 0 .494.064.679-.066.185-.21.582-.256.708-.253.513-.74 1.162-1.672 1.162-.265 0-.587-.118-.733-.22 0 0-.432-.316-.566-.488 0 0-.16-.257-.215-.406 0 0-.11-.363-.11-.728v-3.176c0-.365.05-.642.097-.883.05-.244.325-.465.545-.555.22-.094.714-.206 1.288-.057 0 0 .296-.024.587.22.282.234.358.677.358 1.096v3.174c0 .424-.053.728-.105.925-.052.198-.158.405-.392.645-.242.242-.565.498-1.527.498-.795 0-1.474-.28-1.816-.557-.342-.278-.495-.668-.495-1.04V10.77h3.271v-.001h.002v.001H18.88zm.432-3.769c2.95 0 5.334 2.384 5.334 5.333 0 2.95-2.384 5.333-5.334 5.333-2.95 0-5.333-2.383-5.333-5.333 0-2.949 2.383-5.333 5.333-5.333zm0 1.334c-2.206 0-4 1.794-4 4s1.794 4 4 4c2.206 0 4-1.794 4-4s-1.794-4-4-4z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
-        <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">
-          <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
-            <path
-              fill-rule="evenodd"
-              d="M9.444 2c1.607 0 2.94 1.243 2.94 2.776 0 1.534-1.333 2.777-2.94 2.777C7.838 7.554 6.5 6.311 6.5 4.777 6.5 3.243 7.838 2 9.444 2zm5.713 7.592h1.425v1.532h-1.425v4.59c0 .752-.642 1.36-1.427 1.36H8.036c-.785 0-1.427-.608-1.427-1.36v-4.59H5.183v-1.532h1.426V6.99c0-1.607 1.334-2.778 2.94-2.778.785 0 1.427.608 1.427 1.36v1.942zm-5.712 0h1.425v1.532H9.445zm-2.85 0h1.425v1.532H6.594zm1.425 2.298h1.426v1.531h-1.426zm-2.85 0h1.425v1.531H4.744zm2.85 2.298h1.426v1.53H7.594zm-2.85 0h1.425v1.53H4.744zM9.444 18c-1.606 0-2.939-1.243-2.939-2.777 0-1.533 1.333-2.776 2.94-2.776 1.607 0 2.939 1.243 2.939 2.776 0 1.534-1.332 2.777-2.939 2.777zm6.791-6.408h-1.426v-1.532h1.426zm-2.85 0h-1.426v-1.532h1.426zm-2.85 0H9.504v-1.532H6.594zm-2.85 2.298h-1.426v-1.53h1.426zm2.85 0h-1.426v-1.53h1.426zm2.85 0h-1.426v-1.53h1.426z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
-        <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">
-          <svg viewBox="0 0 30 30" fill="currentColor" class="h-6">
-            <path
-              fill-rule="evenodd"
-              d="M15 0C6.716 0 0 6.716 0 15c0 8.284 6.716 15 15 15 8.284 0 15-6.716 15-15 0-8.284-6.716-15-15-15zm7.007 13.752h-2.738c-.16 0-.305.063-.414.168-.11.105-.17.25-.17.403v4.756c0 .406-.33.736-.736.736h-2.805c-.406 0-.736-.33-.736-.736v-4.756c0-.153-.06-.298-.17-.403-.11-.105-.254-.168-.414-.168H9.993c-.406 0-.736.33-.736.736v4.756c0 .406-.33.736-.736.736h-2.81c-.188 0-.376.072-.514.21-.139.139-.21.326-.21.514v4.777c0 .188.072.376.21.514.139.139.326.21.514.21h11.014c.188 0 .376-.072.514-.21.139-.139.21-.326.21-.514v-4.777c0-.188-.072-.376-.21-.514-.139-.139-.326-.21-.514-.21zm-1.616-6.207c-.115.09-1.795 1.43-2.186 1.772-.01.008-.019.016-.03.024v.03c-.255.212-.538.422-.851.632-.49.334-.91.506-1.383.506-.473 0-.893-.172-1.383-.506-.314-.21-.597-.42-.851-.632v-.03c-.01-.008-.02-.016-.03-.024-.39-.342-2.07-1.682-2.186-1.772-.207-.15-.268-.424-.118-.63.15-.208.423-.268.63-.118.09.065 2.24 1.67 2.628 1.965.008.006.017.012.025.018v.001c.065.048.16.118.28.118.11 0 .204-.07.28-.118v-.001c.008-.006.017-.012.025-.018.388-.295 2.539-1.9 2.628-1.965.206-.15.48-.09.63.118.15.206.09.48-.118.63z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
-      </div>
+  </nav>
+  <main class="ml-10 mt-10">
+    <a href="<?php echo $profileLink; ?>">Deel mijn profiel</a>
+
+    <h1 class="text-5xl mb-10 font-semibold">Profile</h1>
+    <h2 class="text-3xl text-fadedblue mb-5">Hi, it's <?php echo htmlspecialchars($username); ?>!</h2>
+    <?php if($user->checkVerify()): ?>
+      <h2 class="text-2xl text-fadedpurple mb-5">I am a verified user.</h2>
+    <?php endif; ?>
+    <h3 class="text-2xl mb-3"><?php echo "My credits: " . htmlspecialchars($credits);?></h3>
+    <div class="bg-offwhite rounded w-96 p-5">
+      <h3 class="text-2xl mb-3">My biography:</h3>
+      <p class="text-8sm mb-3"><?php echo htmlspecialchars($biography); ?></p> 
     </div>
-  </div>
-</div>
+    <form action="#" method="post">
+      <div>
+        <label class="underline text-fadedblue text-1xl" for="biography">Update your biography here:</label>
+        <input id="biography" name="biography" type="text" placeholder="Biography">
+      </div>
+      <div>
+        <button class="bg-fadedpurple px-5 py-3 mt-5 rounded font-semibold text-white" type="submit" name="updateProfile">Add bio</button>
+      </div>
+    </form>
+
+    <form action="reportedUsers.php" method="post">
+  <input type="hidden" name="reportedUser" value="<?php echo $username; ?>">
+  <button class="bg-red-500 px-5 py-3 mt-5 rounded font-semibold text-white" type="submit" name="reportUser">Report User</button>
+</form>
+
+</main>
+</body>
+<h2>Delete Your Account</h2>
+<p>Please confirm your email address to delete your account and all associated data.</p>
+<form method="post" action="dashboard.php">
+  <label for="email">Email:</label>
+  <input type="email" name="email" required>
+  <br>
+  <label for="confirm_delete">Confirm your email address:</label>
+  <input type="email" name="confirm_delete" required>
+  <br>
+  <p>To delete your account, type "delete my account" below:</p>
+  <input type="text" name="delete_confirmation" required>
+  <br>
+  <button type="submit" name="delete_account" onclick="return confirm('Are you sure you want to delete your account? This action cannot be undone.');">Delete Account</button>
+</form>
+
+<h2>Download Your Data</h2>
+<p>You can download a copy of your data by clicking the button below.</p>
+<a href="download_data.php">Download My Data</a>
+
+<form action="" method="post" enctype="multipart/form-data" class="ml-10">
+  <label>Selecteer een afbeelding:</label>
+  <input type="file" name="image" required>
+  <br><br>
+  <input type="submit" name="uploadPhoto" value="Uploaden" class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded">
+  <input type="submit" name="deletePhoto" value="Verwijderen" class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded">
+</body>
+</html>
+
+<?php
+include('classes/Profile.php');
+
+$profile = new Profile();
+
+if (isset($_POST['uploadPhoto'])) {
+  $result = $profile->setProfilePhoto($_FILES['image']);
+  echo $result;
+}
+
+if (isset($_POST['deletePhoto'])) {
+  $result = $profile->deleteProfilePhoto();
+  echo $result;
+}
+
+if ($profile->getProfilePhoto() != '') {
+  echo '<img src="' . $profile->getProfilePhoto() . '">';
+}
+?>
+
+<?php include_once("footer.php");?>
