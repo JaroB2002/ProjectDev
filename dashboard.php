892a90a0117a2abc32e73d110ee846b1f12290ba
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-05-25 21:55:15 +0200

    chore: styling dashboard pt1

--- dashboard.php
+++ dashboard.php
@@ -4,12 +4,7 @@ include_once("bootstrap.php");
     session_start();
 
     $report = new Report();
-    /*if($report->reportCountPrompt()){
-        $reportCount = $report->deletePrompt(true);
-        echo "done";
-    };*/
     
-    //niet via url binnen raken
     if(!isset($_SESSION['username'])){
         header("location: index.php");
     } 
@@ -42,10 +37,6 @@ include_once("bootstrap.php");
         $search = "all";
     }
 
-    //var_dump($search);
-    
-
-    //$allApprovedPrompts = Prompt::getAllApproved();
     $filter = Prompt::filter($pricing, $type, $date, $search);
     $likes = new Prompt();
     $favorites = new Favorite();
@@ -54,7 +45,6 @@ include_once("bootstrap.php");
         $user = new User();
         if(isset($_GET['buy'])){
             $canBuy = $user->checkIfCanBuy();
-            /*var_dump($canBuy);*/
             if($canBuy['can_buy'] === '1'){
                 $user->buyPrompt();
                 $user->sellPrompt();
@@ -65,6 +55,16 @@ include_once("bootstrap.php");
     } catch (Exception $e) {
         $errorMessage = $e->getMessage();
     }
+
+    // Handle prompt rating
+    if (isset($_POST['rating']) && isset($_POST['promptId'])) {
+        $rating = $_POST['rating'];
+        $promptId = $_POST['promptId'];
+    
+        $prompt = new Prompt();
+        $prompt->ratePrompt($promptId, $rating);
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -98,16 +98,21 @@ include_once("bootstrap.php");
   </script>
 </head>
 
-<body class="mx-10">
-    <nav><?php include_once("navigation.php")?></nav>
-    <h1 class="text-4xl font-semibold">Homepage</h1>
-    <!--search-->
-    <form method="get" action=""> <!--veranderd nr get-->
-        <div>
-            <h2 class="text-xl font-semibold mt-7">Search title or tag</h2>
-            <input name="search" type="text" placeholder="Search by title or tag">
+<body>
+    <header class="bg-gray-900">
+        <nav><?php include_once("navigation.php")?></nav>
+        <div class="flex flex-col justify-center items-center py-20 gap-10">
+            <img src="uploads/homeheader.jpg" alt="header" class="rounded-full w-96 h-96">
+            <img src="uploads/PromptBaes.svg" alt="header" class="w-96 h-35" >
+            <h4 class="text-offgrey text-xl max-w-sm text-center">Promptbaes, the perfect webapp for finding your prompt. Thanks for joining our community, thanks for being a promptbae!</h4>
         </div>
-        <article class="flex flex-row">
+    </header>
+    <form method="get" action="" class="bg-fadedpurple py-10"> 
+        <div class="flex justify-center items-center flex-col">
+            <h2 class="text-xl font-semibold">Search title or tag</h2>
+            <input class="max-w-sm" name="search" type="text" placeholder="Search by title or tag">
+        </div>
+        <article class="flex flex-row justify-center">
         <div class="mr-10">
             <h2 class="text-xl font-semibold mt-7 mb-2">Pricing</h2>
             <select name="price">
@@ -134,138 +139,29 @@ include_once("bootstrap.php");
             </select>
         </div>
         </article>
-        <div>
+        <div class="flex justify-center">
             <button class="bg-fadedblue text-white px-5 py-2 mt-5 rounded font-semibold text-lg" type="submit" value="Search">Search</button>
         </div>
     </form>
-    <?php
-include_once("bootstrap.php");
-
-//session_start();
-
-$report = new Report();
-/*if($report->reportCountPrompt()){
-    $reportCount = $report->deletePrompt(true);
-    echo "done";
-};*/
-
-//niet via url binnen raken
-if (!isset($_SESSION['username'])) {
-    header("location: index.php");
-}
-//price filter
-if (!empty($_GET['price'])) {
-    $pricing = $_GET['price'];
-} else {
-    $pricing = "all";
-}
-//type filter
-if (!empty($_GET['type'])) {
-    $type = $_GET['type'];
-} else {
-    $type = "all";
-}
-//date filter
-if (!empty($_GET['date'])) {
-    $date = $_GET['date'];
-} else {
-    $date = "all";
-}
-//text filter
-if (!empty($_GET['search'])) {
-    $search = $_GET['search'];
-} else {
-    $search = "all";
-}
-
-//$allApprovedPrompts = Prompt::getAllApproved();
-$filter = Prompt::filter($pricing, $type, $date, $search);
-$likes = new Prompt();
-$favorites = new Favorite();
-//$promptId = $_POST["promptId"];
-//var_dump($comments);
-
-try {
-    $user = new User();
-    if (isset($_GET['buy'])) {
-        $canBuy = $user->checkIfCanBuy();
-        /*var_dump($canBuy);*/
-        if ($canBuy['can_buy'] === '1') {
-            $user->buyPrompt();
-            $user->sellPrompt();
-        } else {
-            throw new Exception("You don't have enough credits.");
-        }
-    }
-} catch (Exception $e) {
-    $errorMessage = $e->getMessage();
-}
-
-// Handle prompt rating
-if (isset($_POST['rating']) && isset($_POST['promptId'])) {
-    $rating = $_POST['rating'];
-    $promptId = $_POST['promptId'];
-    
-    $prompt = new Prompt();
-    $prompt->ratePrompt($promptId, $rating);
-}
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-    tailwind.config = {
-        theme: {
-            screens: {
-                sm: '480px',
-                md: '768px',
-                lg: '1024px',
-                xl: '1280px',
-            },
-            extend: {
-                colors: {
-                    fadedpurple: '#C688F4',
-                    fadedblue: '#5C69AA',
-                    offgrey: '#fdfcfd',
-                    offblack: '#313639',
-                    offwhite: '#f9f9f9'
-                }
-            }
-        }
-    }
-    </script>
-</head>
-
-<body class="mx-10">
-    <!-- Rest of your HTML code -->
-
     <h2 class="text-3xl font-semibold mt-5">Prompt overview</h2>
     <?php if ($filter == null): ?>
         <p class="text-xl font-semibold mt-5 text-fadedpurple">No prompts found</p>
     <?php endif; ?>
-    <article class="flex flex-wrap">
+    <article class="flex flex-wrap justify-center mt-5 pt-5">
         <?php foreach ($filter as $prompt): ?>
-            <div class="my-5 bg-offblack mr-10 px-8 py-8 rounded max-w-sm">
+            <div class="my-5 bg-offgrey mr-10 px-8 py-8 rounded max-w-md">
                 <h3 class="font-semibold text-xl text-fadedpurple"><?php echo htmlspecialchars($prompt["name"]); ?></h3>
                 <a href="user.php?id=<?php echo $prompt["email"]; ?>">
-                    <p class="mb-5 text-lg text-offwhite hover:text-fadedpurple"><strong>User:</strong> <?php echo $prompt["email"]; ?></p>
+                    <p class="mb-5 text-lg text-offblack hover:text-fadedpurple"><strong>User:</strong> <?php echo $prompt["email"]; ?></p>
                 </a>
-                <img class="mb-5" src="<?php echo htmlspecialchars($prompt["image"]); ?>" alt="input image">
-                <p class="mb-3 text-lg text-offwhite"><strong>Description:</strong> <?php echo htmlspecialchars($prompt["description"]); ?></p>
-                <p class="mb-3 text-lg text-offwhite"><strong>Type:</strong> <?php echo htmlspecialchars($prompt["type"]); ?></p>
-                <p class="mb-3 text-lg text-offwhite"><strong>Price:</strong> <?php echo htmlspecialchars($prompt["price"]); ?></p>
+                <img class="object-none h-96 w-96 mb-5" src="<?php echo htmlspecialchars($prompt["image"]); ?>" alt="input image">
+                <p class="mb-5 text-lg text-offblack"><strong>Description:</strong> <?php echo htmlspecialchars($prompt["description"]); ?></p>
+                <p class="mb-5 text-lg text-offblack"><strong>Type:</strong> <?php echo htmlspecialchars($prompt["type"]); ?></p>
+                <p class="mb-5 text-lg text-offblack"><strong>Price:</strong> <?php echo htmlspecialchars($prompt["price"]); ?></p>
                 <form method="post" class="mt-4">
                     <input type="hidden" name="promptId" value="<?php echo $prompt['id']; ?>">
                     <div class="flex items-center">
-                        <label for="rating" class="mr-2">Rate prompt:</label>
+                        <label for="rating" class="mb-5 text-lg text-offblack">Rate prompt:</label>
                         <select name="rating" id="rating" class="border border-gray-300 rounded p-1">
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -278,20 +174,20 @@ if (isset($_POST['rating']) && isset($_POST['promptId'])) {
                 </form>
                 <button id="reportButton" data-promptid="<?php echo $prompt['id']; ?>" class="p-3 px-6 pt-2 text-white bg-fadedpurple rounded-full baseline font-semibold text-lg">Report Prompt</button>
                 <div>
-                    <button class="report-button" data-prompt-id="<?php echo $prompt["id"]; ?>" data-error-id="<?php echo 'error-' . $prompt["id"]; ?>">Report user</button>
+                    <!--<button class="report-button" data-prompt-id="<?php echo $prompt["id"]; ?>" data-error-id="<?php echo 'error-' . $prompt["id"]; ?>">Report user</button>-->
                     <?php if (isset($errorMessage) && $_GET["buy"] == $prompt["id"]): ?>
                         <div class="error-message text-red-500" id="<?php echo 'error-' . $prompt["id"]; ?>">
-                            <?php echo $errorMessage; ?>
+                            <?php echo htmlspecialchars($errorMessage); ?>
                         </div>
                     <?php endif; ?>
                     <form action="" class="mt-3">
-                        <button class="bg-fadedpurple px-5 py-3 rounded font-semibold ml-5" type="submit" name="buy" value="<?php echo $prompt['id']; ?>">Buy</button>
+                        <button class="bg-fadedpurple px-5 py-3 rounded font-semibold" type="submit" name="buy" value="<?php echo $prompt['id']; ?>">Buy</button>
                     </form>
                     <div class="mt-5">
-                        <a href="#" data-id="<?php echo $prompt['id']; ?>" class="like bg-fadedpurple px-5 py-3 rounded font-semibold ml-5 mt-5"><?php if (Like::getAll($prompt['id']) == true) { echo 'Unlike '; } else { echo 'Like ';}?> <span class='likes' id="likes"><?php echo $likes->getLikes($prompt['id']) ?> people like this</span> </a>
+                        <a href="#" data-id="<?php echo $prompt['id']; ?>" class="like bg-fadedpurple px-5 py-3 rounded font-semibold mt-5"><?php if (Like::getAll($prompt['id']) == true) { echo 'Unlike '; } else { echo 'Like ';}?> <span class='likes' id="likes"><?php echo $likes->getLikes($prompt['id']) ?> people like this</span> </a>
                     </div>
                     <div class="mt-8">
-                        <a href="#" data-id="<?php echo $prompt['id']; ?>" class="favorite bg-fadedpurple px-5 py-3 rounded font-semibold ml-5 mt-5"><?php if (Favorite::getAll($prompt['id']) == true) { echo 'remove from favorites '; } else { echo 'add to favorites ';}?></a>
+                        <a href="#" data-id="<?php echo $prompt['id']; ?>" class="favorite bg-fadedpurple px-5 py-3 rounded font-semibold mt-5"><?php if (Favorite::getAll($prompt['id']) == true) { echo 'remove from favorites '; } else { echo 'add to favorites ';}?></a>
                     </div>
                     <form action="" class="mt-10 flex rounded">
                         <input class="text" type="text" placeholder="write a comment".>
@@ -308,8 +204,7 @@ if (isset($_POST['rating']) && isset($_POST['promptId'])) {
             </div>
         <?php endforeach; ?>
     </article>
-
-    <!--<script src="index.css"></script>-->
+    <footer><?php include_once("footer.php");?></footer>
     <!--<script src="app.js"></script>-->
     <script>
         let report = document.querySelectorAll("#reportButton");
@@ -384,20 +279,17 @@ if (isset($_POST['rating']) && isset($_POST['promptId'])) {
         });
     }
     </script>
-</body>
-</html>
-
-    </script>
-      <script src="js/favorite.js"></script>
-      <script src="js/comment.js"></script>
+    <script src="js/favorite.js"></script>
+    <script src="js/comment.js"></script>
 </body>
 </html>
 <?php
+//IN COMMENTS GEZET WANT MOET NOG GECLEAND WORDEN*/
 // START DELETE ACCOUNT CODE
 
 // Establish database connection
 //$conn = new PDO('mysql:host=localhost;dbname=demo', 'root', '');
-$conn = Db::getInstance();
+/*$conn = Db::getInstance();
 
 // Check if form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -437,6 +329,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
     }
-}
+}*/
 ?>
-<?php include_once("footer.php");?>
+
