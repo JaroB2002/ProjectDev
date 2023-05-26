748671d3f7644023dd5b049a13eb7631258ec404
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-05-26 12:34:42 +0200

    fix: right path to js

--- dashboard.php
+++ dashboard.php
@@ -206,38 +206,6 @@ include_once("bootstrap.php");
         <?php endforeach; ?>
     </article>
     <footer><?php include_once("footer.php");?></footer>
-    <!--<script src="app.js"></script>-->
-    <script>
-        let report = document.querySelectorAll("#reportButton");
-
-        report.forEach(function(button){
-            button.addEventListener("click", reportPrompt);
-        });
-
-        function reportPrompt(event){
-            console.log(event);
-            event.preventDefault();
-            console.log("reportPrompt");
-            let promptid = event.target.dataset.promptid;
-            console.log(promptid);
-            let formData = new FormData();
-            formData.append("promptid", promptid);
-
-            let item = this;
-            fetch("ajax/reportPrompt.php", {
-                method: "POST",
-                body: formData
-            })
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(result){
-                if(result.status == "success"){
-                    item.innerHTML = result.message;
-                }
-            });
-        }
-    </script>
     <script>
         let links = document.querySelectorAll(".like");
         for (let i = 0; i < links.length; i++) {
@@ -282,6 +250,7 @@ include_once("bootstrap.php");
     </script>
     <script src="js/favorite.js"></script>
     <script src="js/comment.js"></script>
+    <script src="js/reportPrompt.js"></script>
 </body>
 </html>
 <?php
