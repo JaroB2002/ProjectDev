0b082ece4da1d5e2a094b67a45a46e08f89b1acf
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-05-28 11:09:37 +0200

    chore: improved nav

--- navigation.php
+++ navigation.php
@@ -1,38 +1,3 @@
-<?php
-//https://play.tailwindcss.com/no3sgy5KXk
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-PmkEJHmZvcwdeUDzL5Z+K9QGQxxbivn5nMxvM5rPLnAR">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-    tailwind.config = {
-      theme: {
-        screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-        },
-        extend: {
-          colors: {
-            fadedpurple: '#C688F4',
-            fadedblue: '#5C69AA',
-            offgrey: '#faf9f6',
-            offblack: '#313639',
-            offwhite: '#f9f9f9'
-          }
-        }
-      }
-    }
-  </script>
-</head>
-<body>
 <nav class="relative container mx-auto p-6 bg-offgrey rounded-md">
     <div class="flex items-center justify-between">
         <div class="hidden md:flex space-x-6">
@@ -61,12 +26,3 @@
         <a href="logout.php" class="block p-2 text-white bg-fadedpurple font-bold">Log out</a>
     </div>
 </nav>
-
-<script>
-    function toggleNavMenu() {
-        const navMenu = document.getElementById('navMenu');
-        navMenu.classList.toggle('hidden');
-    }
-</script>
-</body>
-</html>
